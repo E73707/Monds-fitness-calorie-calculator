c0eeb5b9fb06f8e606829844b8f94be5c01d0a5b
E73707 <94885681+E73707@users.noreply.github.com>
2023-01-24 17:35:20 +1100

    added most of the functionality,, also changed to grid format, the calculator works as intended, just need to make some clight changes

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,12 @@ let activityEl = $(".activityEl");
 let maintenanceEl = $(".maintenance");
 let sessionsPerWeekEL = $(".sessions-per-week");
 let sessionIntensityEl = $(".session-intensity");
-let maintenance;
+let maintenanceMacrosEl = $(".maintenance-macros");
+let loseEl = $(".lose");
+let loseMacrosEl = $(".lose-macros");
+let gainEl = $(".gain");
+let gainMactrosEl = $(".gain-macros");
+let inputBtnEl = $(".input-button");
 
 let user = {
   age: undefined,
@@ -22,6 +27,7 @@ let user = {
 sessionIntensityEl.click(function (event) {
   event.preventDefault();
   user.averageSessionIntensity = this.value;
+  console.log(user.averageSessionIntensity);
 });
 
 sessionsPerWeekEL.click(function (event) {
@@ -45,14 +51,32 @@ sessionsPerWeekEL.click(function (event) {
 });
 
 submitEl.click(function (event) {
+  $(document).ready(function () {
+    for (const key in user) {
+      console.log(user[key], key);
+      if (user[key] == undefined || "") {
+        console.log(key);
+        return $("#my-toast").toast("show");
+      }
+    }
+  });
+
   event.preventDefault();
   user.age = ageEl.val();
   user.height = heightEl.val();
   user.weight = weightEl.val();
 
-  maintenanceEl.text(
-    `${calculateCals() - 50} - ${calculateCals() + 50}   calories`
-  );
+  let maintenanceCals = calculateCals();
+  maintenanceEl.text(`${maintenanceCals - 50} - ${maintenanceCals + 50} Kcals`);
+  maintenanceMacrosEl.text(`${maintenanceMacros(maintenanceCals)}`);
+
+  let loseCals = maintenanceCals - maintenanceCals * 0.2;
+  loseEl.text(`${loseCals - 50} - ${loseCals + 50} Kcals`);
+  loseMacrosEl.text(`${loseMacros(loseCals)}`);
+
+  let gainCals = maintenanceCals * 1.1;
+  gainEl.text(`${gainCals - 50} - ${gainCals + 50} Kcals`);
+  gainMactrosEl.text(`${gainMacros(gainCals)}`);
 });
 
 let calculateCals = function () {
@@ -64,32 +88,32 @@ let calculateCals = function () {
     var femaleBmr = 10 * user.weight + 6.25 * user.height - 5 * user.age - 161;
   }
 
-  if (user.averageSessionIntensity == "low") {
+  if (user.averageSessionIntensity == "1") {
     sessionWeekIntensity = lowSessionIntensityFunc();
-  } else if (user.averageSessionIntensity == "moderate") {
+  } else if (user.averageSessionIntensity == "2") {
     sessionWeekIntensity = midSessionIntensityFunc();
-  } else if (user.averageSessionIntensity == "high") {
+  } else if (user.averageSessionIntensity == "3") {
     sessionWeekIntensity = midSessionIntensityFunc();
   }
 
   if (maleBmr) {
-    if (user.exerciseActivity == "sedentary") {
+    if (user.exerciseActivity == "1") {
       maintenance = sedentaryFunc(maleBmr) + sessionWeekIntensity;
-    } else if (user.exerciseActivity == "light active") {
+    } else if (user.exerciseActivity == "2") {
       maintenance = lightActiveFunc(maleBmr) + sessionWeekIntensity;
-    } else if (user.exerciseActivity == "active") {
+    } else if (user.exerciseActivity == "3") {
       maintenance = activeFunc(maleBmr) + sessionWeekIntensity;
-    } else if (user.exerciseActivity == "very active") {
+    } else if (user.exerciseActivity == "4") {
       maintenance = VeryActiveFunc(maleBmr) + sessionWeekIntensity;
     }
   } else if (femaleBmr) {
-    if (user.exerciseActivity == "sedentary") {
+    if (user.exerciseActivity == "1") {
       maintenance = sedentaryFunc(femaleBmr) + sessionWeekIntensity;
-    } else if (user.exerciseActivity == "light active") {
+    } else if (user.exerciseActivity == "2") {
       maintenance = lightActiveFunc(femaleBmr) + sessionWeekIntensity;
-    } else if (user.exerciseActivity == "active") {
+    } else if (user.exerciseActivity == "3") {
       maintenance = activeFunc(femaleBmr) + sessionWeekIntensity;
-    } else if (user.exerciseActivity == "very active") {
+    } else if (user.exerciseActivity == "4") {
       maintenance = VeryActiveFunc(femaleBmr) + sessionWeekIntensity;
     }
   }
@@ -123,3 +147,46 @@ let midSessionIntensityFunc = function () {
 let highSessionIntensityFunc = function () {
   return (250 * user.sessionsPerWeek) / 7;
 };
+
+let maintenanceMacros = function (calories) {
+  let leftover = calories;
+  let proteinGrams = Math.round(user.weight * 2);
+  let proteinCals = proteinGrams * 4;
+  let fatGrams = Math.round((calories * 0.25) / 9);
+  let fatCals = calories * 0.25;
+  leftover = leftover - proteinCals - fatCals;
+  let carbGrams = Math.round(leftover / 4);
+  return `P: ${proteinGrams} C: ${carbGrams} F: ${fatGrams}`;
+};
+
+let loseMacros = function (calories) {
+  let leftover = calories;
+  let proteinGrams = Math.round(user.weight * 2.2);
+  let proteinCals = proteinGrams * 4;
+  let fatGrams = Math.round((calories * 0.25) / 9);
+  let fatCals = calories * 0.25;
+  leftover = leftover - proteinCals - fatCals;
+  let carbGrams = Math.round(leftover / 4);
+  return `P: ${proteinGrams} C: ${carbGrams} F: ${fatGrams}`;
+};
+
+let gainMacros = function (calories) {
+  let leftover = calories;
+  let proteinGrams = Math.round(user.weight * 1.8);
+  let proteinCals = proteinGrams * 4;
+  let fatGrams = Math.round((calories * 0.3) / 9);
+  let fatCals = calories * 0.25;
+  leftover = leftover - proteinCals - fatCals;
+  let carbGrams = Math.round(leftover / 4);
+  return `P: ${proteinGrams} C: ${carbGrams} F: ${fatGrams}`;
+};
+
+// inputBtnEl.click((event) => {
+//   console.log(event.target.style.backgroundColor);
+//   if (event.target.style.backgroundColor == "rgb(200, 198, 198)") {
+//     console.log("true");
+//     event.target.style.backgroundColor = "#f8f9fa";
+//     return;
+//   }
+//   event.target.style.backgroundColor = "#C8C6C6";
+// });
